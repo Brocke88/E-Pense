4a83f9f969d28734ae7a7548a8f1316ea1f3b331
Atizaz Ahmed <155964172+Brocke88@users.noreply.github.com>
2026-04-18 16:20:54 +0500

    Add files via upload

---
+++ goals.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'dart:io';
+import 'dart:convert';
+
+import 'package:softec_app/state_management/cubit.dart';
+
+class Goals_Records extends StatefulWidget {
+  const Goals_Records({super.key});
+
+  @override
+  State<Goals_Records> createState() => _Goals_RecordsState();
+}
+
+class _Goals_RecordsState extends State<Goals_Records> {
+  @override
+  Widget build(BuildContext context) {
+    final projectStateInstance = BlocProvider.of<project_state>(context);
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: projectStateInstance.historyRecords[2].values.length,
+          itemBuilder: (BuildContext context,int index){
+            return Container(
+              width: 500,
+              height: 200,
+              decoration: BoxDecoration(
+
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10.0),
+                    bottomRight: Radius.circular(10.0),
+                  ),
+                  color: Colors.blue,
+                  // shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 6),
+                        spreadRadius: 3,
+                        color:Color(0xff13095c)
+                    ),
+                    BoxShadow(
+
+                    ),
+                  ]
+              ),child: Center(
+              child: Text(projectStateInstance.historyRecords[2].values.elementAt(index).toString(),
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize:30,
+                  // fontWeight: ,
+                  // fontFamily: ,
+                ),textAlign: TextAlign.center,
+              ),
+            ),
+            );
+          }),
+    );
+  }
+}
